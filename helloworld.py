f9cb0cedb44e063d2155f3e735db9ba5ce49df27
grecolink2 <juliocramirez220490@gmail.com>
2026-09-06 16:59:44 +0000

    NICE

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,10 @@
+import random
 print("hello world")
 import numpy as np
 array = np.array([1,2,3,4])
 print (array)
+dice = [1,2,3,4,5,6]
+
+print(random.choice(dice))
+print(dice)
+random.choice(dice)
