50274342b21b9b3fc071504f0ac355b74c92b502
grecolink2 <juliocramirez220490@gmail.com>
2026-09-13 13:57:30 +0000

    NICE

--- helloworld.py
+++ helloworld.py
@@ -8,3 +8,20 @@ dice = [1,2,3,4,5,6]
 print(random.choice(dice))
 print(dice)
 random.choice(dice)
+
+player_a = domino
+domino = side_a
+domino = side_b
+Side_a = [0,1,2,3,4,5,6]
+Side_b = [0,1,2,3,4,5,6]
+print(random.choice(domino))
+print(random.choice(side_a))
+print(random.choice(side_b))
+player_b = domino
+domino = side_a
+domino = side_b
+Side_a = [0,1,2,3,4,5,6]
+Side_b = [0,1,2,3,4,5,6]
+print(random.choice(domino))
+print(random.choice(side_a))
+print(random.choice(side_b))
